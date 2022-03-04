77c72565fb7a19d771dbb697972973de83216314
NCT-NCT <ncthanh12a0@gmail.com>
2022-03-04 08:30:55 +0700

    Layout

---
+++ gesture/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      theme: ThemeData( primarySwatch: Colors.blue,),
+      home: const MyHomePage(title: 'Home page'),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  // user defined function
+  void _showDialog(BuildContext context) {
+    // user defined function void _showDialog(BuildContext context) {
+    // flutter defined function
+    showDialog(
+      context: context, builder: (BuildContext context) {
+      // return object of type Dialog
+      return AlertDialog(
+        title: const Text("Message"),
+        content: const Text("Hello World"),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text("Close"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title),),
+      body: Center(
+          child: GestureDetector(
+              onTap: () {
+                _showDialog(context);
+              },
+              child: const Text( 'Hello World', )
+          )
+      ),
+    );
+  }
+}
